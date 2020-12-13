9b700a8a48d110aab4d841d238a427733d26cc8f
Allison Howard <allisonhoward309@gmail.com>
2020-12-13 17:25:19 -0600

    Studio 16

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Controllers
@@ -11,7 +12,7 @@ namespace CodingEventsDemo.Controllers
     public class EventCategoryController : Controller
     {
         private EventDbContext context;
-        public EventCategoryController (EventDbContext dbContext)
+        public EventCategoryController(EventDbContext dbContext)
         {
             context = dbContext;
         }
@@ -19,6 +20,29 @@ namespace CodingEventsDemo.Controllers
         {
             List<EventCategory> eventCategory = context.EventCategory.ToList();
             return View(eventCategory);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+        [HttpPost]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.EventCategory.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+            return View("Create", addEventCategoryViewModel);
         }
     }
 }
